b7ef069503f886cda5deaddf3f828766f61927b3
MarkFede1997 <marcofedele1997@live.it>
2020-11-04 11:59:47 +0100

    implementing Junit test, ordinaCrescente tested

--- EsameMarcoFedele/src/test/java/INGSW/EsameMarcoFedele/MyListUtilTest.java
+++ EsameMarcoFedele/src/test/java/INGSW/EsameMarcoFedele/MyListUtilTest.java
@@ -1,15 +1,43 @@
 package INGSW.EsameMarcoFedele;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MyListUtilTest {
 
 	private static MyListUtil ordinatore;
+	private static ArrayList<Integer> lista;
 	
 	@BeforeClass
-	public void prepare()
+	public static void prepare()
 	{
 		ordinatore = new MyListUtil();
+	}
+	
+	@Before
+	public void preparelist()
+	{
+		lista = new ArrayList<Integer>();
+		lista.add(1);
+		lista.add(3);
+		lista.add(2);
+		lista.add(4);
+		lista.add(6);
+		lista.add(5);
+		lista.add(8);
+		lista.add(7);
+		
+	}
+	
+	@Test
+	public void ordinaCrescenteWorks()
+	{
+		assertEquals(Arrays.asList(1,2,3,4,5,6,7,8), ordinatore.ordinaCrescente(lista));
 	}
 
 }
